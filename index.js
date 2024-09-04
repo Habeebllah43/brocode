a0772aa6cc517fca8e59d4eb343d865606b66abb
Habeebllah43 <habeebllah43@gmail.com>
2024-09-04 07:34:49 +0100

    deploy

---
+++ index.js
@@ -0,0 +1,9 @@
+// console.log(`hello`);
+// console.log('i like pizza!');
+
+// window.alert(`drop me`)
+// window.alert('give me breast')
+// window.alert('MAKE IT DONE')
+
+document.getElementById('bestie').innerHTML="barakat"
+document.getElementById('promo').textContent="tv"
