0f650d90ce7325cd32d4e952600d7f50cf8becaa
Habeebllah43 <habeebllah43@gmail.com>
2024-10-30 16:25:23 +0100

    object

---
+++ object.js
@@ -0,0 +1,20 @@
+const person1={
+    firstname:'alo',
+    lastname:'alonge',
+    age:31,
+    Ismarried:true,
+sayminame: ()=>
+  console.log('im habeeb , aiming to become the best dev in the world'),
+    
+eat:function () {
+    console.log("i'm eating amala and ewedu");
+    
+}
+}
+
+
+
+person1.sayminame();
+person1.eat();
+console.log(person1.firstname);
+console.log(person1.Ismarried);
